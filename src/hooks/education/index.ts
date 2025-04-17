92ac8f82a0a66b23722f961678e52c8610003e2a
Lordsisodia <130863457+Lordsisodia@users.noreply.github.com>
2025-04-17 01:00:53 +0100

    Delete src/hooks/education directory

--- src/hooks/education/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './use-educators-list';
-export * from './use-educator-details';
-export * from './use-educator-videos';
-export * from './use-featured-videos';
-export * from './use-educator-correlated-videos';
-export * from './use-video-categories';
-export * from './types';
